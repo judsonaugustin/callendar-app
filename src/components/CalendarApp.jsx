84c799461a67698c70cf0e3eb851bd3892e13ee0
judsonaugustin <111638501+judsonaugustin@users.noreply.github.com>
2023-06-09 11:02:25 +0530

    Update CalendarApp.jsx

--- src/components/CalendarApp.jsx
+++ src/components/CalendarApp.jsx
@@ -28,29 +28,29 @@ const CalendarApp = () => {
           <div className="mt-2">
             <EventForm calendarRef={calendarRef} />
           </div>
-          <div>
-            <div className="mx-4 mt-8">
-              <h3 className="text-3xl font-semibold text-center mt-4 mb-4">
-                Events
-              </h3>
-              *{" "}
-              <div className="h-64 overflow-y-auto bg-white rounded-lg p-4">
-                {events.length > 0 ? (
-                  events.map((event, index) => (
-                    <div key={index}>
-                      <p className="font-bold text-xl mt-1">{event.title}</p>
-                      <div className="flex gap-4">
-                        <p>{event.start}</p>
-                        <p>{event.end}</p>
-                      </div>
-                    </div>
-                  ))
-                ) : (
-                  <p>No events yet</p>
-                )}
-              </div>
-            </div>
-          </div>
+//           <div>
+//             <div className="mx-4 mt-8">
+//               <h3 className="text-3xl font-semibold text-center mt-4 mb-4">
+//                 Events
+//               </h3>
+//               {" "}
+//               <div className="h-64 overflow-y-auto bg-white rounded-lg p-4">
+//                 {events.length > 0 ? (
+//                   events.map((event, index) => (
+//                     <div key={index}>
+//                       <p className="font-bold text-xl mt-1">{event.title}</p>
+//                       <div className="flex gap-4">
+//                         <p>{event.start}</p>
+//                         <p>{event.end}</p>
+//                       </div>
+//                     </div>
+//                   ))
+//                 ) : (
+//                   <p>No events yet</p>
+//                 )}
+//               </div>
+//             </div>
+//           </div>
         </div>
         <div className="w-full flex">
           <div className=" bg-white mt-auto mb-4 w-full py-4 px-4 border-t-8 border-yellow-200 rounded-3xl">
